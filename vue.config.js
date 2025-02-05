8f9baa283359b4fdc30563a7cfa0ced0970c4db7
SuzanneB836 <6035697@mborijnland.nl>
2025-02-05 13:25:26 +0100

    update for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/vue-test/'  // Replace 'vue-test' with your GitHub repository name
 })
